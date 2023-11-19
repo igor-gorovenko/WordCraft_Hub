e4b82ceb9c571bb31e7967a1ffbe5a3cbbc773f3
igorforspeak@gmail.com <igorforspeak@gmail.com>
2023-11-19 15:07:34 +0200

    del welcome page

--- app/Http/Controllers/WordController.php
+++ app/Http/Controllers/WordController.php
@@ -3,13 +3,12 @@
 namespace App\Http\Controllers;
 
 use App\Models\Word;
-use Illuminate\Http\Request;
 
 class WordController extends Controller
 {
     public function index()
     {
         $words = Word::all();
-        return view('word.index', ['words' => $words]);
+        return view('index', ['words' => $words]);
     }
 }
