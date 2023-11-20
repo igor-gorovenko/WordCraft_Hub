ad22fbca5f12add3e624fadff14e2505b5596170
igorforspeak@gmail.com <igorforspeak@gmail.com>
2023-11-20 14:07:04 +0200

    add tags in detail word

--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -7,6 +7,14 @@
     <h6>Word: {{ $word->word }}</h6>
     <h6>Translation: {{ $word->translation }}</h6>
     <h6>Usage count: {{ $word->usage_count }}</h6>
+    <h6>Tags:
+        @foreach($word->tags as $tag)
+        {{ $tag->name }}
+        @unless($loop->last)
+        ,
+        @endunless
+        @endforeach
+    </h6>
 </div>
 
 @endsection
