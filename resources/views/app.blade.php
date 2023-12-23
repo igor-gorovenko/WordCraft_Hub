54c3f10fa143211501e4c638c151da9df79f914c
igorforspeak@gmail.com <igorforspeak@gmail.com>
2023-12-23 18:13:11 +0200

    upd templates, title and logo name

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -2,14 +2,14 @@
 <html>
 
 <head>
-    <title>WordCraft Hub</title>
+    <title>Word Craft Hub</title>
     <!-- Styles -->
     <link rel="stylesheet" href="{{ asset('build/assets/main-5ffef9fa.css') }}">
 </head>
 
 <body>
     <div class="container">
-        <h1><a href="{{ route('index') }}">WordCraft Hub</a></h1>
+        <h1><a href="{{ route('index') }}">Word Craft Hub</a></h1>
         @yield('content')
     </div>
 </body>
